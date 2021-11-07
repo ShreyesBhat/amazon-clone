6b479dd6e6d9a76ef136e934fb10ad5e566a18e8
Shreyes Bhat <shreyespb@gmail.com>
2021-11-07 13:44:22 -0500

    Initial push

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCPTPE5YlCHD4NOyXCRssPKM5MnDsVhzpY",
+    authDomain: "clone-efc67.firebaseapp.com",
+    projectId: "clone-efc67",
+    storageBucket: "clone-efc67.appspot.com",
+    messagingSenderId: "218127246286",
+    appId: "1:218127246286:web:92989481c61416a23f019f",
+    measurementId: "G-Y88FDDSBX1"
+  };
